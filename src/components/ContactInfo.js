bad76d320af9e7985da85c93389b2446db7f447a
PiraTechnics <devin1younge@gmail.com>
2023-05-31 11:35:57 -0700

    Cojntact Info form and basic styling

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,31 +1,36 @@
 import React, { Component } from "react";
+import "../styles/style.css";
 
 class ContactInfo extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      emailText: "",
-    };
-  }
-
-  handleChange = (e) => {};
-
   render() {
-    const { emailVal } = this.props;
+    const { nameVal, addrVal, cityVal, countryVal, phoneVal, emailVal } =
+      this.props;
 
     return (
       <div>
         <h2>Contact</h2>
-        <form>
-          <label htmlFor="emailField"></label>
-          <input
-            id="emailField"
-            value={emailVal}
-            onChange={this.handleChange}
-            type="text"
-          />
-        </form>
+        <fieldset>
+          <form>
+            <div className="inputGroup">
+              <label htmlFor="nameField">Name: </label>
+              <input id="nameField" value={nameVal} type="text" />
+            </div>
+            <div className="inputGroup">
+              <label htmlFor="addrField">Address: </label>
+              <input id="addrField" value={addrVal} type="text" />
+              <label htmlFor="cityField">City: </label>
+              <input id="cityField" value={cityVal} type="text" />
+              <label htmlFor="countryField">Country: </label>
+              <input id="countryField" value={countryVal} type="text" />
+            </div>
+            <div className="inputGroup">
+              <label htmlFor="phoneField">Phone: </label>
+              <input id="phoneField" value={phoneVal} type="text" />
+              <label htmlFor="emailField">Email: </label>
+              <input id="emailField" value={emailVal} type="text" />
+            </div>
+          </form>
+        </fieldset>
       </div>
     );
   }
